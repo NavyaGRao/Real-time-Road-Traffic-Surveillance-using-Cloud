6130466b0bbfa918573d496587a0633f97e1690c
Navya G <navyabgrao@gmail.com>
2020-04-05 15:55:01 -0600

    Updated

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ VCS entry point.
 
 import time
 import cv2
+import boto3
 
 from dotenv import load_dotenv
 load_dotenv()
 
 import settings
 from util.logger import init_logger
-from util.image import take_screenshot
 from util.logger import get_logger
-from util.debugger import mouse_callback
 from VehicleCounter import VehicleCounter
 
 init_logger()
@@ -58,8 +57,8 @@ def run():
     if record:
         # initialize video object to record counting
         output_video = cv2.VideoWriter(settings.OUTPUT_VIDEO_PATH, \
-                                        cv2.VideoWriter_fourcc(*'MJPG'), \
-                                        30, \
+                                        cv2.VideoWriter_fourcc(*'mp4v'), \
+                                        27.58, \
                                         (f_width, f_height))
 
     logger.info('Processing started.', extra={
@@ -79,29 +78,29 @@ def run():
         },
     })
 
-    if not headless:
+    #if not headless:
         # capture mouse events in the debug window
-        cv2.namedWindow('Debug')
-        cv2.setMouseCallback('Debug', mouse_callback, {'frame_width': f_width, 'frame_height': f_height})
+        #cv2.namedWindow('Debug')
+        #cv2.setMouseCallback('Debug', mouse_callback, {'frame_width': f_width, 'frame_height': f_height})
 
-    is_paused = False
-    output_frame = None
+    #is_paused = False
+    #output_frame = None
 
     # main loop
     while is_cam or cap.get(cv2.CAP_PROP_POS_FRAMES) + 1 < cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        k = cv2.waitKey(1) & 0xFF
-        if k == ord('p'): # pause/play loop if 'p' key is pressed
-            is_paused = False if is_paused else True
-            logger.info('Loop paused/played.', extra={'meta': {'label': 'PAUSE_PLAY_LOOP', 'is_paused': is_paused}})
-        if k == ord('s') and output_frame is not None: # save frame if 's' key is pressed
-            take_screenshot(output_frame)
-        if k == ord('q'): # end video loop if 'q' key is pressed
-            logger.info('Loop stopped.', extra={'meta': {'label': 'STOP_LOOP'}})
-            break
+        #k = cv2.waitKey(1) & 0xFF
+        #if k == ord('p'): # pause/play loop if 'p' key is pressed
+            #is_paused = False if is_paused else True
+            #logger.info('Loop paused/played.', extra={'meta': {'label': 'PAUSE_PLAY_LOOP', 'is_paused': is_paused}})
+        #if k == ord('s') and output_frame is not None: # save frame if 's' key is pressed
+         #   take_screenshot(output_frame)
+        #if k == ord('q'): # end video loop if 'q' key is pressed
+         #   logger.info('Loop stopped.', extra={'meta': {'label': 'STOP_LOOP'}})
+          #  break
 
-        if is_paused:
-            time.sleep(0.5)
-            continue
+        #if is_paused:
+         #   time.sleep(0.5)
+          #  continue
 
         _timer = cv2.getTickCount() # set timer to calculate processing frame rate
 
@@ -115,7 +114,7 @@ def run():
             if not headless:
                 debug_window_size = settings.DEBUG_WINDOW_SIZE
                 resized_frame = cv2.resize(output_frame, debug_window_size)
-                cv2.imshow('Debug', resized_frame)
+                cv2.imwrite('Debug.png', resized_frame)
 
         processing_frame_rate = round(cv2.getTickFrequency() / (cv2.getTickCount() - _timer), 2)
         frames_processed = round(cap.get(cv2.CAP_PROP_POS_FRAMES))
@@ -141,5 +140,53 @@ def run():
     logger.info('Processing ended.', extra={'meta': {'label': 'END_PROCESS', 'counts': vehicle_counter.counts}})
 
 
+    a=str(logger)
+    print(a)
+    b = (a.split(" ",2)[1])
+    print(b)
+
+    n = 0
+    m = 0
+    C = 0
+    mylines = []
+    with open ('./data/logs/'+b+'.log', 'rt') as myfile:
+        for line in myfile:
+            mylines.append(line)
+            n = n+1
+        A = mylines[n-1].split()
+        for D in A:
+            C = C + 1
+    B = A.index('"counts":')
+    final_word = A[B:C]
+    print(final_word)
+    f = open('my_file', 'w')
+    f.write(str(final_word))
+    f.close()
+    s3=boto3.client('s3')
+    bucketlog = 'vidconvert'
+    bucketvideoavi = 'cf-templates-ckxl87y565ea-us-east-1'
+    s3.upload_file('./data/logs/'+b+'.log',bucketlog,'logs/'+b+'.log')
+    s3.upload_file('my_file', 'test-mary2','my-file.txt')
+
+    data1 = open('data/videos/output.avi', 'rb')
+    s32=boto3.resource('s3')
+
+    #s32.Bucket(bucket).put_object(Key='videos/'b'.avi', Body=data1,ContentType='video/avi')
+    s32.Bucket(bucketvideoavi).put_object(Key='output.avi', Body=data1,ContentType='video/avi')
+
+
+
 if __name__ == '__main__':
     run()
+
+
+
+
+
+
+
+
+
+
+
+
